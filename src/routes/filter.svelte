012401b2fbb69d597b9575424490b21af2226af2
Jacob <jrmadams02@gmail.com>
2022-09-28 20:14:12 -0400

    filter

--- src/routes/filter.svelte
+++ src/routes/filter.svelte
@@ -1,12 +1,17 @@
 <div class="w-96 h-full bg-white">
-    <div class="text-3xl">
+    <div class="text-3xl font-semibold ml-5">
         Term
     </div>
-    <hr class="m-5">
+    <hr class="mb-5 mr-5 ml-5">
     <div class="flex w-full justify-center">
         <div class="text-center rounded-full bg-base-100 w-20 mr-auto ml-auto"> A Term</div>
         <div class="text-center rounded-full bg-base-100 w-20 mr-auto ml-auto"> B Term</div>
         <div class="text-center rounded-full bg-base-100 w-20 mr-auto ml-auto"> C Term</div>
         <div class="text-center rounded-full bg-base-100 w-20 mr-auto ml-auto"> D Term</div>
     </div>
+    <div class="text-3xl font-semibold ml-5 mt-5">
+        Listings
+    </div>
+    <hr class="mb-5 mr-5 ml-5">
+
 </div>
