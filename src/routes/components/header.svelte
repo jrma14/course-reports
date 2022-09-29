30bf1e23bda5c86ff8dfc90c85ab5f0a4ae518e9
Pollock, Nathan <ndpollock@wpi.edu>
2022-09-29 13:01:08 -0400

    random stuff

--- src/routes/components/header.svelte
+++ src/routes/components/header.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Logo from './logo.svelte';
+	import { page } from '$app/stores';
+
 </script>
 
 <div class="w-full h-16 bg-primary flex text-white items-center text-xl">
 	<Logo />
-	<div class="ml-5 underline">Courses</div>
-	<div class="ml-5">Professors</div>
+	<a href="/" class="ml-5 {$page.url.pathname === "/" ? "underline" : ""}">Courses</a>
+	<a href="/professors" class="ml-5 {$page.url.pathname === "/professors" ? "underline" : ""}">Professors</a>
 </div>
