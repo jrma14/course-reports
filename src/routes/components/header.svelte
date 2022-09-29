50978b6e24a5c8fc9796f5eb3e412834b90bfac3
Michael Emerson <emerson.michael12@gmail.com>
2022-09-29 12:15:57 -0400

    update header styling

--- src/routes/components/header.svelte
+++ src/routes/components/header.svelte
@@ -2,7 +2,7 @@
 	import Logo from './logo.svelte';
 </script>
 
-<div class="w-full h-28 bg-primary flex text-white items-center text-3xl">
+<div class="w-full h-16 bg-primary flex text-white items-center text-xl">
 	<Logo />
 	<div class="ml-5 underline">Courses</div>
 	<div class="ml-5">Professors</div>
