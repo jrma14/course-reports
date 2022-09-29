e6a3cce625457a67c7b94c08ff59bd259070fead
Jacob <jrmadams02@gmail.com>
2022-09-28 23:07:44 -0400

    cleaned up layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,10 @@
 <script>
     import "../app.css";
-    import Logo from './components/logo.svelte'
+    import Header from './components/header.svelte'
   </script>
   
   <div class="w-screen h-screen bg-base-100">
-    <div class="w-full h-28 bg-primary flex text-white items-center text-3xl">
-      <Logo/>
-      <div class="ml-5 underline">
-        Courses
-      </div>
-      <div class="ml-5">
-        Professors
-      </div>
-    </div>
+    <Header />
     <slot />
-
   </div>
   
---
+++ src/routes/components/header.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Logo from './logo.svelte';
+</script>
+
+<div class="w-full h-28 bg-primary flex text-white items-center text-3xl">
+	<Logo />
+	<div class="ml-5 underline">Courses</div>
+	<div class="ml-5">Professors</div>
+</div>
