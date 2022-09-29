544e5a84bcc478aee8d6d673a72ffe23f70aab1c
Michael Emerson <emerson.michael12@gmail.com>
2022-09-29 13:20:22 -0400

    Added scroll to filter

--- src/routes/components/filter.svelte
+++ src/routes/components/filter.svelte
@@ -19,7 +19,7 @@
 	}
 </script>
 
-<div class="min-w-[384px] w-96 h-full bg-white select-none drop-shadow-md">
+<div class="min-w-[384px] w-96 h-full bg-white select-none drop-shadow-md overflow-y-scroll">
 	<div class="text-3xl font-semibold ml-5 mt-3">Term</div>
 	<hr class="mb-5 mr-5 ml-5 mt-1" />
 	<div class="flex w-full justify-center">
