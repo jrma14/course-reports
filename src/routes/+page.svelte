b79382539b945d740f27908f6ff98e86bf37c1b7
Pollock, Nathan <ndpollock@wpi.edu>
2022-09-28 20:22:50 -0400

    search bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </script>
 
 
-    <div class="w-80 h-full flex">
-        <Filter/>
-<SearchBar class="w-full" on:search={(e) => console.log(e.detail)} />
+    <div class="h-full w-full flex">
+        <Filter />
+        <SearchBar class="w-full" on:search={(e) => console.log(e.detail)} />
     </div>
--- src/routes/components/SearchBar.svelte
+++ src/routes/components/SearchBar.svelte
@@ -11,10 +11,10 @@
 
 </script>
 
-<div class="form-control">
-    <div class="input-group">
-      <input type="text" placeholder="Search…" class="input input-bordered" bind:value={searchQuery} on:keyup={runSearch} />
-      <button class="btn btn-square" on:click={runSearch}>
+<div class="form-control m-3 w-full">
+    <div class="input-group w-full">
+      <input type="text" placeholder="Search…" class="input input-bordered w-full bg-white" bind:value={searchQuery} on:keyup={runSearch} />
+      <button class="btn btn-square bg-white" on:click={runSearch}>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
       </button>
     </div>
