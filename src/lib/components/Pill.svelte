361fd40b192cbde8511cce652bcd8e0ecb56cc9e
Jacob <jrmadams02@gmail.com>
2022-09-30 11:28:53 -0400

    sveltified the pills

--- src/lib/components/Pill.svelte
+++ src/lib/components/Pill.svelte
@@ -3,6 +3,7 @@
 	export let checked = false;
 	export let size = 'large';
 
+	
 	const uncheckedStyles = `flex justify-center items-center rounded-full bg-base-100 ${
 		size === 'sm' ? 'w-20' : 'w-40 h-8'
 	} mr-auto ml-auto hover:cursor-pointer hover:bg-primary/50`;
@@ -10,13 +11,10 @@
 		size === 'sm' ? 'w-20' : 'w-40 h-8'
 	} mr-auto ml-auto hover:cursor-pointer`;
 	let pillClasses = uncheckedStyles;
-
-	function clickedPill() {
-		checked = !checked;
-		pillClasses = checked ? checkedStyles : uncheckedStyles;
-	}
+	
+	$: pillClasses = checked? checkedStyles: uncheckedStyles
 </script>
 
-<div on:click={clickedPill} class={pillClasses}>
+<div on:click={() => {checked = !checked}} class={pillClasses}>
 	{text}
 </div>
