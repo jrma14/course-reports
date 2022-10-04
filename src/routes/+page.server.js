a7d0c032851e9226219a242b4fac4ce784bde1ff
Jacob <jrmadams02@gmail.com>
2022-10-03 23:27:54 -0400

    b

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,13 +1,14 @@
 import mysql from 'mysql2/promise'
 import { error } from '@sveltejs/kit'
 import { DATABASE_URL } from '$env/static/private'
-const connection = await mysql.createPool({
-    uri: DATABASE_URL,
-    connectionLimit: 10
-})
+
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
+    const connection = await mysql.createPool({
+        uri: DATABASE_URL,
+        connectionLimit: 10
+    })
     console.time('db')
     let [rows] = await connection.query('SELECT course_title FROM courses LIMIT 10')
     console.timeEnd('db')
