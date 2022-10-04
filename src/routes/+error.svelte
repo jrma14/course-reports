958e04dc1033f4b36b70751c021deda73f4a1fa9
Pollock, Nathan <ndpollock@wpi.edu>
2022-10-03 22:26:16 -0400

    error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,9 +1,24 @@
 <script>
 	import '../app.css';
+
+	//import page store
+	import { page } from '$app/stores';
+
+	function getErrorText() {
+		switch ($page.status) {
+			case 404:
+				return 'Page not found';
+			case 500:
+				return 'Internal server error';
+			default:
+				return 'Unknown error';
+		}
+	}
 </script>
 
-<h1 class="text-6xl text-center m-3">404 - Page not found</h1>
+<h1 class="text-6xl text-center m-3">
+	{$page.status}
+</h1>
 <p class="text-center m-3">
-	The page you are looking for might have been removed, had its name changed, or is temporarily
-	unavailable.
+	{getErrorText()}
 </p>
