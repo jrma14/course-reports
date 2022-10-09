1a5f0ab6c455469b949297ac89ac83408df19068
Pollock, Nathan <ndpollock@wpi.edu>
2022-10-09 01:40:08 -0400

    course page layout

---
+++ src/routes/courses/[coursenumber]/components/OverallHeader.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let title, number;
+</script>
+
+<div class="flex flex-col items-baseline w-full m-5">
+	<div class="flex flex-row items-baseline gap-5">
+		<h1 class="text-5xl font-bold">{title}</h1>
+		<h1 class="text-2xl font-light">{number}</h1>
+	</div>
+	<!-- horizontal line -->
+	<div class="w-full h-1 bg-gray-300 my-1" />
+</div>
+<h1 class="text-center w-full text-5xl">Overall</h1>
