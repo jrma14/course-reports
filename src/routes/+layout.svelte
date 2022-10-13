3bfe9123321e73fcf00e813fe078e7086d607c15
Michael <47831816+memerson12@users.noreply.github.com>
2022-10-13 00:37:00 -0400

    add title and meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,15 @@
 	import Header from '$lib/components/header.svelte';
 </script>
 
+<svelte:head>
+	<title>WPI Course Reports</title>
+	<meta
+		name="description"
+		content="A one stop stop for choosing your next courses. Look up courses to easaliy see their rankings and how different teachers compare for that course. "
+	/>
+	<meta name="keywords" content="WPI, Course Reports, OSCAR, Course Ratings" />
+</svelte:head>
+
 <div class="w-screen h-screen bg-base-100" data-theme="mytheme">
 	<Header />
 	<slot />
