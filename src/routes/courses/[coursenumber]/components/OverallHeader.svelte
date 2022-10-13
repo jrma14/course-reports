e21eb4c02d734bcb1244f4e77041f22c8505136e
Pollock, Nathan <ndpollock@wpi.edu>
2022-10-12 23:19:44 -0400

    bar chart and margins

--- src/routes/courses/[coursenumber]/components/OverallHeader.svelte
+++ src/routes/courses/[coursenumber]/components/OverallHeader.svelte
@@ -2,9 +2,9 @@
 	export let title, number;
 </script>
 
-<div class="flex flex-col items-baseline w-full m-5">
+<div class="flex flex-col items-baseline w-full m-0">
 	<div class="flex flex-row items-baseline gap-5">
-		<h1 class="text-5xl font-bold">{title}</h1>
+		<h1 class="text-5xl font-bold mt-3 ml-3">{title}</h1>
 		<h1 class="text-2xl font-light">{number}</h1>
 	</div>
 	<!-- horizontal line -->
