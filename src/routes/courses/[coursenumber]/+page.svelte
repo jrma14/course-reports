f5e884827c3edeadc597cf801d799a6fda9a646e
Jacob <jrmadams02@gmail.com>
2022-10-12 21:27:34 -0400

    chartjs

--- src/routes/courses/[coursenumber]/+page.svelte
+++ src/routes/courses/[coursenumber]/+page.svelte
@@ -1,9 +1,8 @@
 <script>
 	import OverallHeader from './components/OverallHeader.svelte';
 	import { onMount } from 'svelte';
-
+	import { Scatter } from 'svelte-chartjs';
 	import * as d3 from 'd3';
-	import { scaleLinear } from 'd3-scale';
 
 	import Logo from '../../../lib/components/logo.svelte';
 	export let data;
@@ -84,84 +83,6 @@
 		temp.push(prof);
 		dataset1.push(temp);
 	}
-	// console.log('dataset', dataset1);
-
-	const margin = { top: 10, right: 30, bottom: 30, left: 60 },
-		width = 460 - margin.left - margin.right,
-		height = 400 - margin.top - margin.bottom;
-
-	// console.log(profGradeVsHrs);
-	onMount(() => {
-		const svg = d3
-			.select('#my_dataviz')
-			.append('svg')
-			.attr('width', '100%')
-			.attr('height', '100%')
-			.style('display', 'flex')
-			// .style('margin-left', '100px')
-			.append('g')
-			.attr('x', '50%')
-			.attr('y', '50%')
-			.attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-		const x = d3.scaleLinear().domain([0, 5]).range([0, width]);
-		svg.append('g').attr('transform', `translate(0, ${height})`).call(d3.axisBottom(x));
-
-		// Add Y axis
-		const y = d3.scaleLinear().domain([0, 5]).range([height, 0]);
-		svg.append('g').call(d3.axisLeft(y));
-
-		// Add dots
-		svg
-			.append('g')
-			.selectAll('dot')
-			.data(dataset1)
-			.join('circle')
-			.attr('cx', function (d) {
-				return x(d[1]);
-			})
-			.attr('cy', function (d) {
-				return y(d[0]);
-			})
-			.attr('r', 5)
-			.attr('class', 'scatterdot')
-			.style('fill', '#69b3a2');
-
-		// console.log(dataset1);
-		console.log(svg.selectAll('.scatterdot'));
-		svg
-			.append('g')
-			.selectAll('circle')
-			.data(dataset1)
-			.join('text')
-			.text((d) => {
-				console.log(d);
-				return d[2];
-			})
-			.attr('x', function (d) {
-				return x(d[1]);
-			})
-			.attr('y', function (d) {
-				return y(d[0]);
-			})
-			.style('font-size', '20px')
-			.style('text-color', 'black');
-
-		svg
-			.append('text')
-			.attr('text-anchor', 'end')
-			.attr('transform', 'rotate(-90)')
-			.attr('y', -margin.left + 20)
-			.attr('x', -margin.top)
-			.text('Expected Grade');
-
-		svg
-			.append('text')
-			.attr('text-anchor', 'end')
-			.attr('x', width)
-			.attr('y', height + margin.top + 20)
-			.text('Hours/Week');
-	});
 
 	//update profAvgs to have the average rating
 	for (let prof in profAvgs) {
@@ -181,71 +102,6 @@
 	for (let i = 0; i < profs.length; i++) {
 		profColors[profs[i]] = colorScale(i);
 	}
-
-	onMount(() => {
-		const xTicks = [0, 1, 2, 3, 4, 5];
-		const yTicks = profs;
-		const padding = { top: 20, right: 15, bottom: 20, left: 25 };
-		let width = el.parentElement.clientWidth;
-		let height = el.parentElement.clientHeight;
-		// get bounding box height
-		let elParent = el.parentElement;
-		let parentHeight = elParent.getBoundingClientRect().height;
-		console.log(parentHeight);
-		//calculate margin
-		//min margin is 10px
-		//estimate margin to be 10px
-		let totalMargin = 5 * profs.length;
-		let barHeight = Math.floor(parentHeight - totalMargin) / profs.length;
-
-		let margin = 5;
-		//recalculate bar height to account for margin
-
-		d3.select(el)
-			.selectAll('div')
-			.data(profs)
-			.enter()
-			.append('div')
-			.attr('class', 'bar')
-			.style('width', (d) => {
-				//calc width based on avg rating and parent width
-				return (profAvgs[d]['avgRating'] / 5) * el.getBoundingClientRect().width + 'px';
-			})
-			.style('height', (d) => {
-				//calc height based on number of professors and parent height
-				return barHeight + 'px';
-			})
-			.style('max-height', (d) => {
-				//calc height based on number of professors and parent height
-				return '70px';
-			})
-			.style('background-color', (d) => {
-				return profColors[d];
-			})
-			.style('border-radius', '0px 10px 10px 0px')
-			.style('margin-bottom', (d) => {
-				//if last element, no margin
-				if (d == profs[profs.length - 1]) {
-					return '0px';
-				}
-				return margin + 'px';
-			})
-			.text((d) => {
-				return d;
-			})
-			.style('min', 'white')
-			//left align text
-			.style('text-align', 'left')
-			.style('color', 'white')
-			// add black stroke to the text
-			.style('text-shadow', '-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black')
-			// center the text vertically
-			.style('display', 'flex')
-			.style('align-items', 'center')
-			.style('font-size', (d) => {
-				return '15px';
-			});
-	});
 </script>
 
 <div class="flex flex-col h-full font-bold text-3xl w-full">
@@ -280,7 +136,7 @@
 		<!-- Expected grade / hrs per week section -->
 		<div class="w-[62%] m-3 h-full bg-white rounded-2xl text-center">
 			Grade / Hours
-			<div class="w-full h-full flex justify-center items-center" id="my_dataviz" />
+			<Scatter {dataset1} options={{ responsive: true }} />
 		</div>
 	</div>
 </div>
